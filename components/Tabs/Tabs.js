9bfbd127f49c6201a0952aab5163a980fbd37f9f
Moses Samuel <mosamuel98@gmail.com>
2019-04-11 12:38:08 -0700

    add TabLink constructor

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,18 +2,19 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = element
     
     // Get the custom data attribute on the Link
-    // this.data;
+    this.data = this.element.dataset.tab
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab${this.data}]`)
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = TabItem.of(this.itemElement)
     
     // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', this.select.bind(this))
 
   };
 
@@ -33,6 +34,10 @@ class TabLink {
 }
 
 class TabItem {
+  static of(element) {
+    return new TabItem(element)
+  }
+
   constructor(element) {
     // Assign this.element to the passed in element
     // this.element;
